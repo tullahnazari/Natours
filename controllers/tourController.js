fedd5722b75a7383bae4cf2ffaa658ef5a64c305
tnaz <tullah.nazari@gmail.com>
2019-09-11 21:37:37 -0500

    Paraming middleware

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -5,6 +5,17 @@ const tours =JSON.parse(
     fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
     );
 
+exports.checkID = (req, res, next, val) => {
+    if (req.params.id * 1 > tours.length)
+      {
+          return res.status(404).json({
+              status: 'fail',
+              message: 'Invalid ID'
+          });
+      }
+      next();
+}
+
 exports.getAllTours = (req, res) => {
     console.log(req.requestTime);
     res.status(200).json({
@@ -62,13 +73,6 @@ exports.createTour = (req, res) => {
 
 exports.updateTour = (req, res) => {
 
-    if (req.params.id * 1 > tours.length)
-      {
-          return res.status(404).json({
-              status: 'fail',
-              message: 'Invalid ID'
-          });
-      }
       res.status(200).json({
           status: 'success',
           data: {
@@ -78,14 +82,6 @@ exports.updateTour = (req, res) => {
     };
 
 exports.deleteTour = (req, res) => {
-
-    if (req.params.id * 1 > tours.length)
-      {
-          return res.status(404).json({
-              status: 'fail',
-              message: 'Invalid ID'
-          });
-      }
       res.status(204).json({
           status: 'success',
           data: {
