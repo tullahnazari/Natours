471e2e151131ff785c3b13ebb489d15a0d11fd96
tnaz <tullah.nazari@gmail.com>
2019-09-14 10:56:22 -0500

    settting up db

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
+const mongoose = require('mongoose');
 //env variables
 const dotenv = require('dotenv');
 dotenv.config({path: './config.env'});
 //Everything not related to express doesn't go in app.js (env var, server connection, etc)
 const app = require('./app');
+
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true, 
+    useFindAndModify: false
+}).then(() => console.log("DB Connection homie!"));
 
 
 const port = process.env.PORT || 8000;
