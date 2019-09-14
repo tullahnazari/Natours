62f77d2e357df0b11d59513c78f2e779e013ec8b
tnaz <tullah.nazari@gmail.com>
2019-09-14 11:16:24 -0500

    setting up schema and model

--- server.js
+++ server.js
@@ -14,6 +14,26 @@ mongoose
     useFindAndModify: false
 }).then(() => console.log("DB Connection homie!"));
 
+//DB Schema
+const tourSchema = new mongoose.Schema({
+    name: {
+        type: String, 
+        required: [true, 'A tour must have a name'],
+        unique: true
+    },
+    rating: {
+        type: Number,
+        default: 4.5
+    },
+    price: {
+        type: Number,
+        required: [true, 'A tour must have a price']
+    }
+});
+
+//DB Model from Schema
+const Tour = mongoose.model('Tour', tourSchema);
+
 
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
