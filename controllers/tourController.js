4dfa55d663861f877c081b899de0e1f5d97bd113
tnaz <tullah.nazari@gmail.com>
2019-09-16 19:15:08 -0500

    Matching And Grouping

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -9,7 +9,6 @@ exports.aliasTopTours = (req, res, next) => {
     next();
 };
 
-
 //get all documents in collection
 exports.getAllTours = async (req, res) => {
   try {
@@ -114,3 +113,43 @@ exports.deleteTour = async (req, res) => {
     
     }
     }; 
+
+    exports.getTourStats = async (req, res) => {
+        try{
+            const stats = await Tour.aggregate([
+                {
+                    $match: { ratingsAverage: { $gte: 4.5 } }
+                },
+                { 
+                $group: {
+                    _id: { $toUpper: '$difficulty' },
+                    num: { $sum: 1},
+                    numRatings: { $sum: '$ratingsQuantity'},
+                    avgRating:{ $avg: '$ratingsAverage'},
+                    avgPrice: { $avg: '$price'},
+                    minPrice: { $min: '$price'},
+                    maxPrice: { $max: '$price'},
+
+
+                }
+
+                },
+                {
+                    $sort: { avgPrice: 1 }
+                }
+
+            ]);
+            res.status(200).json({
+                status: 'success',
+                data: {
+                    stats
+                }
+            });
+
+        } catch (err){
+            res.status(404).json({
+                status: 'fail',
+                message: err
+            });
+        }
+    }
