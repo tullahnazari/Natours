abb484e320d6354e442a71cfe95acb420ccfc363
tnaz <tullah.nazari@gmail.com>
2019-09-18 18:00:20 -0500

    Handling Unhandled Routes

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
+//serving static files MW
+app.use(express.static(`${__dirname}/public`));
 //using middleware (middle of request and response)
 app.use(express.json());
 //Morgan middleware and enviroment added
@@ -12,10 +14,14 @@ if (process.env.NODE_ENV === 'development') {
 app.use(morgan('dev'));
 }
 
-//serving static files MW
-app.use(express.static(`${__dirname}/public`));
-
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+//should be all the way at the bottom because of order of execution(it handles all)
+app.all('*', (req, res, next) => {
+    res.status(404).json({
+        status: 'fail',
+        message: `Can't find ${req.originalUrl} on this server`
+    });
+});
 
 module.exports = app;
