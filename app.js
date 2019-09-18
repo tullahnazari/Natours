a0102658172547fd1b841faf90badc1a127c0512
tnaz <tullah.nazari@gmail.com>
2019-09-18 18:18:56 -0500

    Global Error Handling MW

--- app.js
+++ app.js
@@ -18,9 +18,23 @@ app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 //should be all the way at the bottom because of order of execution(it handles all)
 app.all('*', (req, res, next) => {
-    res.status(404).json({
-        status: 'fail',
-        message: `Can't find ${req.originalUrl} on this server`
+    // res.status(404).json({
+    //     status: 'fail',
+    //     message: `Can't find ${req.originalUrl} on this server`
+    // });
+    const err = new Error(`Can't find ${req.originalUrl} on this server`);
+    err.status = 'fail';
+    err.statusCode = 404;
+    next(err);
+});
+
+app.use((err, req, res, next) => {
+    err.statusCode = err.statusCode || 500;
+    err.status = err.status || 'error'
+
+    res.status(err.statusCode).json({
+        status: err.status,
+        message: err.message
     });
 });
 
