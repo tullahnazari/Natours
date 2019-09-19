a927563690265ccdf885023684857f0d333d3c0e
tnaz <tullah.nazari@gmail.com>
2019-09-18 21:02:47 -0500

    Refactoring and structuring global error handler

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const morgan = require('morgan');
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
+
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
@@ -18,24 +21,10 @@ app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 //should be all the way at the bottom because of order of execution(it handles all)
 app.all('*', (req, res, next) => {
-    // res.status(404).json({
-    //     status: 'fail',
-    //     message: `Can't find ${req.originalUrl} on this server`
-    // });
-    const err = new Error(`Can't find ${req.originalUrl} on this server`);
-    err.status = 'fail';
-    err.statusCode = 404;
-    next(err);
+    next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
 
-app.use((err, req, res, next) => {
-    err.statusCode = err.statusCode || 500;
-    err.status = err.status || 'error'
+app.use(globalErrorHandler);
 
-    res.status(err.statusCode).json({
-        status: err.status,
-        message: err.message
-    });
-});
 
 module.exports = app;
