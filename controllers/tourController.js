424bb873e3bd130a9fc8b864fdb6f3e3aca076e4
tnaz <tullah.nazari@gmail.com>
2019-09-21 11:37:48 -0500

    404 error messages :)

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,6 +1,7 @@
 const Tour = require('./../model/tourModel');
 const APIFeatures = require('./../utils/apiFeatures');
 const catchAsync = require('./../utils/catchAsync');
+const AppError = require('./../utils/appError');
 
 
 
@@ -37,6 +38,10 @@ exports.getTour = catchAsync(async (req, res, next) => {
 
     const tour = await Tour.findById(req.params.id);
 
+    if (!tour) {
+        return next(new AppError("No tour found with that ID", 404))
+    }
+
     res.status(200).json({
         status: 'success',
         data: {
@@ -65,6 +70,10 @@ exports.updateTour = catchAsync(async (req, res, next) => {
         runValidators: true
     });
 
+    if (!tour) {
+        return next(new AppError("No tour found with that ID", 404))
+    }
+
     res.status(200).json({
         status: 'success',
         data: {
@@ -75,6 +84,10 @@ exports.updateTour = catchAsync(async (req, res, next) => {
 
 exports.deleteTour = catchAsync(async (req, res, next) => {
         const tour = await Tour.findByIdAndDelete(req.params.id);
+
+        if (!tour) {
+            return next(new AppError("No tour found with that ID", 404))
+        }
     
         res.status(204).json({
             status: 'success',
