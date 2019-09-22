744b33fa13b6b965446f4dad9fd3999d67588df0
tnaz <tullah.nazari@gmail.com>
2019-09-21 20:37:43 -0500

    Handling Invalid Databasae IDs

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,3 +1,9 @@
+const AppError = require('./../utils/appError');
+
+const handleCastErrorDB = err => {
+    const message = `Invalid ${err.path}: ${err.value}.`;
+    return new AppError(message, 400);
+};
 
 const sendErrorDev = (err, res) => {
     res.status(err.statusCode).json({
@@ -39,8 +45,10 @@ module.exports = (err, req, res, next) => {
         sendErrorDev(err, res)
        
     } else if (process.env.NODE_ENV === 'production'){
+    let error = {...err};
+    if (error.name === 'CastError')  error = handleCastErrorDB(error)
 
-        sendErrorProd(err, res);
+        sendErrorProd(error, res);
 
     }
 };
