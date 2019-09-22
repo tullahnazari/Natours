d4b6daf8dedd6023268bdbc9517bff4211c27564
tnaz <tullah.nazari@gmail.com>
2019-09-21 20:15:04 -0500

    Error during development and production

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,12 +1,47 @@
 
+const sendErrorDev = (err, res) => {
+    res.status(err.statusCode).json({
+        status: err.status,
+        message: err.message,
+        error: err,
+        stack: err.stack
+
+    });
+
+    
+}
+
+const sendErrorProd = (err, res) => {
+
+//operational, trusted error: send message to client
+if(err.isOperational) {
+    res.status(err.statusCode).json({
+        status: err.status,
+        message: err.message
+    });
+//programming or other unknown errorL dont leak error detail 
+} else {
+    console.error('Error 🥵', err);
+    res.status(500).json({
+        status: 'error',
+        message: 'something went wrong'
+    })
+}
+};
 
 module.exports = (err, req, res, next) => {
     console.log(err.stack)
     err.statusCode = err.statusCode || 500;
     err.status = err.status || 'error'
 
-    res.status(err.statusCode).json({
-        status: err.status,
-        message: err.message
-    });
+    if(process.env.NODE_ENV === 'development'){
+
+        sendErrorDev(err, res)
+       
+    } else if (process.env.NODE_ENV === 'production'){
+
+        sendErrorProd(err, res);
+
+    }
 };
+
