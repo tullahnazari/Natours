f2cd844ca21c01fe8eb475e36149fdc748615007
tnaz <tullah.nazari@gmail.com>
2019-09-28 21:08:00 -0500

    Setting Security HTTP HEaders by using helmet middleware

--- app.js
+++ app.js
@@ -3,31 +3,38 @@ const morgan = require('morgan');
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
-//Global Morgan middleware and enviroment added
+//////GLOBAL MIDDLEWARES AND THEIR NAMES\\\\\\\
+
+//Set security HTTP headers mw
+app.use(helmet());
+
+//Development logging mw
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
     }
 
-//limits the amount of calls from singular IP address in x time (100 requests in 1 hour)
+//limits the amount of calls from singular IP address in x time (100 requests in 1 hour) mw
 const limiter = rateLimit({
     max: 5000,
     windowMs: 60* 60 * 1000,
     message: 'Too many requests from this IP, please try again in an hour'
 });
-
 app.use('/api', limiter);
 
-//serving static files MW
+//Serving static files mw
 app.use(express.static(`${__dirname}/public`));
-//using middleware (middle of request and response)
-app.use(express.json());
 
+//Body parser, reading data from body into req.body mw
+app.use(express.json({ limit: '10kb' }));
+
+//Test Middleware
 app.use((req, res, next) => {
     req.requstTime = new Date().toISOString();
     next();
