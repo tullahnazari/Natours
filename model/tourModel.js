f084e826a9e3f284dfde66d2b705493fb32d145b
tnaz <tullah.nazari@gmail.com>
2019-10-03 14:32:07 -0500

    Added indexing for better read performance on id & name

--- model/tourModel.js
+++ model/tourModel.js
@@ -122,6 +122,10 @@ const tourSchema = new mongoose.Schema({
     toObject: { virtuals: true }
 });
 
+//indexing results for read performance
+tourSchema.index( { price: 1, ratingsAverage: -1 } );
+tourSchema.index({ slug: 1 });
+
 tourSchema.virtual('durationWeeks').get(function(){
     return this.duration / 7;
 });
